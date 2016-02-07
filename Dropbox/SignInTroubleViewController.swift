dea22a24b30b96e454a5b6d8025e62b3a117da1a
dnedkova <dnedkova@lyft.com>
2016-02-07 13:42:07 -0800

    adding files

---
+++ Dropbox/SignInTroubleViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SignInTroubleViewController.swift
+//  Dropbox
+//
+//  Created by Diana Nedkova on 2/6/16.
+//  Copyright © 2016 Diana Nedkova. All rights reserved.
+//
+
+import UIKit
+
+class SignInTroubleViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    @IBAction func cancelSignInTroubleView(sender: AnyObject) {
+        
+        self.dismissViewControllerAnimated(true, completion: {})
+    }
+
+}
