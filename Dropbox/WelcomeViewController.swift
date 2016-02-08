faff796f9ae710f8540323c706e2fdc155a4bbd9
dnedkova <dnedkova@lyft.com>
2016-02-07 22:40:26 -0800

    welcome slide flow

--- Dropbox/WelcomeViewController.swift
+++ Dropbox/WelcomeViewController.swift
@@ -8,28 +8,30 @@
 
 import UIKit
 
-class WelcomeViewController: UIViewController {
+class WelcomeViewController: UIViewController, UIScrollViewDelegate  {
+    
+
+    @IBOutlet weak var welcomeScrollView: UIScrollView!
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        welcomeScrollView.pagingEnabled = true
+        
+        welcomeScrollView.contentSize = CGSize(width: 960, height: 568)
+
+        welcomeScrollView.delegate = self
+     
     }
+    
+    
+    
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
